28636d0c6089bc0936f0b712daff558bbf8f7b51
jse <taxsilver331@gmail.com>
2024-11-20 17:29:56 +0900

    blogV@ jpa ã끝

--- src/main/java/com/example/blog/board/BoardRepository.java
+++ src/main/java/com/example/blog/board/BoardRepository.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
@@ -14,32 +15,45 @@ public class BoardRepository {
     // JPA는 EntityManager로 DB에 접근한다 (자바에서 DBConnection)
     private final EntityManager em;
 
-    public void update(int id, String title, String content) {
+    //조회만 하면 업데이트가 알아서 되기 때문에 업데이트 필요없다.
+/*    public void update(int id, String title, String content) {
         Query q = em.createNativeQuery("update board_tb set title=?, content=? where id=?");
         q.setParameter(1, title);
         q.setParameter(2, content);
         q.setParameter(3, id);
         q.executeUpdate();
-    }
+    }*/
 
     public void delete(int id){
-        Query q = em.createNativeQuery("delete from board_tb where id=?");
+/*        Query q = em.createNativeQuery("delete from board_tb where id=?");
         q.setParameter(1, id);
-        q.executeUpdate(); // insert, update, delete 때 사용함
+        q.executeUpdate(); // insert, update, delete 때 사용함*/
+        em.createQuery("delete from Board b where id = :id") // ?대신 :사용한다.
+                .setParameter("id", id)
+                .executeUpdate();
     }
 
     public void save(Board board){
+        // 비영속
         em.persist(board);
+        // 동기화 완료(영속화됨)
+        //return board;
     }
 
-    public List<Board> findAll(){
-        Query q = em.createNativeQuery("select * from board_tb order by id desc", Board.class);
-        return q.getResultList();
+    public List<Board> findAll() {
+/*        Query q = em.createNativeQuery("select * from board_tb order by id desc", Board.class);
+        return q.getResultList();*/
+//        em.createQuery("select b from Board - 클래스 b");
+        return em.createQuery("select b from Board b order by b.id desc ",Board.class)
+                .getResultList();
     }
 
-    public Board findById(int id) {
+/*    public Board findById(int id) {
         Query q = em.createNativeQuery("select * from board_tb where id = ?", Board.class);
         q.setParameter(1, id); // 물음표 완성하기 (물음표 순서, 물음표에 바인딩될 변수값)
         return (Board) q.getSingleResult();
+        }
+ */ public Optional<Board> findById(int id) {
+     return Optional.ofNullable(em.find(Board.class, id));
     }
 }
--- src/main/java/com/example/blog/board/BoardService.java
+++ src/main/java/com/example/blog/board/BoardService.java
@@ -22,12 +22,14 @@ public class BoardService {
     }
 
     public BoardResponse.UpdateFormDTO 게시글수정화면보기(int id) {
-        Board board = boardRepository.findById(id);
+        Board board = boardRepository.findById(id)
+                .orElseThrow( () -> new RuntimeException("해당 id의 게시글이 없습니다 : "+id));
         return new BoardResponse.UpdateFormDTO(board);
     }
 
     public BoardResponse.DetailDTO 게시글상세보기(int id) {
-        Board board = boardRepository.findById(id);
+        Board board = boardRepository.findById(id)
+                .orElseThrow( () -> new RuntimeException("해당 id의 게시글이 없습니다 : "+id));
         return new BoardResponse.DetailDTO(board);
     }
 
@@ -44,6 +46,10 @@ public class BoardService {
     @Transactional
     public void 게시글수정하기(int id, BoardRequest.UpdateDTO updateDTO) {
         // 1. 게시글 조회, 2. 게시글 수정 권한, 3. 게시글 수정, 4. 수정된 엔티티 응답
-        boardRepository.update(id, updateDTO.getTitle(), updateDTO.getContent());
+        //boardRepository.update(id, updateDTO.getTitle(), updateDTO.getContent());
+        Board board = boardRepository.findById(id)
+                .orElseThrow( () -> new RuntimeException("해당 id의 게시글이 없습니다 : "+id)); // 1. 조회
+        board.update(updateDTO.getTitle(), updateDTO.getContent());
+        // 영속화된 객체상태 변경 - update + commit => 더티체킹
     }
 }
